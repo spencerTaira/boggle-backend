abd6f177e552d0f056387157b788f8795fe036d0
Spencer Taira <spencermtaira@gmail.com>
2023-03-03 15:41:31 -0800

    added new route to join a room with a username and updated database

--- routes/static/room.py
+++ routes/static/room.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify, request
 from database import db
 from models.room import Room
+from models.player import Player
+from models.player_in_room import PlayerInRoom
 from app import bcrypt
 room = Blueprint("room", __name__)
 
@@ -27,7 +29,7 @@ def create_room():
     existing = Room.query.get(room_name)
     if existing:
         return ("Error, room exists", 403)
-    
+
     pw_hash = bcrypt.generate_password_hash(password).decode("utf-8")
     print("what is pw_hash>>>>>>>>>>>>>>>", pw_hash)
     room = Room(
@@ -37,9 +39,9 @@ def create_room():
         game_length=game_length,
         private=True, #this is default may want to allow for non-private games later
     )
-    
+
     try:
-    
+
         db.session.add(room)
         db.session.commit()
         #update open rooms visible to other connected clients
@@ -48,28 +50,87 @@ def create_room():
     except Exception as e:
         db.session.rollback()
         return ("Error creating room", 403)
-    
+
 @room.get("/enter")
 def enter_room():
     """
-        Join a room
-        
+        Enter a room
+
         Input: JSON Like:
         {
             roomName: 'test room',
             password: 'password',
         }
     """
-    
+
     room_name = request.args["roomName"]
     password = request.args["password"]
-    
+
     room = Room.query.get(room_name)
     if not room:
         return("Room/password incorrect", 403)
-    
+
     if bcrypt.check_password_hash(room.password, password):
         return (jsonify(roomName=room_name), 200)
     else:
         return ("Room/password incorrect", 403)
-    
+
+@room.post("/join")
+def join_room():
+    """
+        Join a room
+
+        Input: JSON like:
+        {
+            playerName: 'testPlayer',
+            roomId: 'room100'
+        }
+    """
+
+    player_name = request.json['playerName']
+    room_name = request.json['roomId']
+
+    player = Player(
+        name = player_name,
+    )
+
+    room = Room.query.get(room_name)
+
+    if not room:
+        return ("Room does not exist")
+
+    if room.curr_players == room.max_players:
+        return ("Room is full")
+
+    #TODO: set creator or host if first person to join room
+
+    try:
+        room.players.append(player)
+        room.curr_players += 1
+        db.session.commit()
+    except:
+        db.session.rollback()
+        return ("Error creating player")
+
+    if not room.host:
+        print('ROOM HOST ----------------------------------------', room.host)
+        room.host = player.id
+        db.session.commit()
+
+    # updated_room = Room.query.get(room_name)
+
+    room_data = {
+        'roomName': room.room_name,
+        'currPlayers': room.curr_players,
+        'maxPlayers': room.max_players,
+        'host': room.host,
+    }
+
+    return (jsonify(roomData=room_data), 201)
+
+
+
+
+
+
+
