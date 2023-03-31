83a4131773125460b779c049aa16e853cfa77c12
Spencer Taira <spencermtaira@gmail.com>
2023-03-30 17:13:00 -0700

    added a create player route

--- routes/static/player.py
+++ routes/static/player.py
@@ -4,27 +4,29 @@ from models.lobby import Lobby
 from models.player import Player
 from models.player_in_lobby import PlayerInLobby
 from app import bcrypt
+from sqlalchemy import exc
+
 player = Blueprint("player", __name__)
 
 @player.get("/")
 def get_player():
     """
     Gets a players data
-        Input: 
+        Input:
             {
                 playerId: 1
             }
-            
+
         Output:
             {
                 playerId:1,
-                playeName: test
+                playerName: test
             }
     """
-    
+
     player_id = request.args["playerId"]
     player = Player.query.get(player_id)
-    
+
     if player:
         player_data = {
             "playerId": player.id,
@@ -33,6 +35,41 @@ def get_player():
         return (jsonify(playerData=player_data), 200)
     else:
         return (jsonify(error=f"Player {player_id} not found."), 404)
-        
-    
-    
+
+@player.post("/")
+def create_player():
+    """
+    Create a player in the database
+        Input:
+            {
+                playerName: 'player 1'
+            }
+
+        Output:
+            {
+                playerId: 1,
+                playerName: 'player 1'
+            }
+    """
+
+    player_name = request.json['playerName']
+    player = Player(
+        name=player_name
+    )
+
+    try:
+        db.session.add(player)
+        db.session.commit()
+    except exc.SQLAlchemyError as e:
+        print('Error reason', e.args[0])
+        if 'value too long' in e.args[0]:
+            db.session.rollback()
+            return (jsonify(error="Name is too long. Please do not exceed 50 chars"), 400)
+        return (jsonify(error="Unknown Error Maybe Server???"), 500)
+
+    player_data = {
+        "playerId": player.id,
+        "playerName": player.name
+    }
+
+    return (jsonify(playerData=player_data), 201)
