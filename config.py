4a90dbd948599ec865373c8d16cb846e4dbfd7ce
Spencer Taira <spencermtaira@gmail.com>
2023-04-27 14:30:01 -0700

    commented out database test u rl

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.environ['DATABASE_URL']
-DATABASE_URL_TEST = os.environ['DATABASE_URL_TEST']
+# DATABASE_URL_TEST = os.environ['DATABASE_URL_TEST']
 SECRET_KEY = "this-is-a-secret"
